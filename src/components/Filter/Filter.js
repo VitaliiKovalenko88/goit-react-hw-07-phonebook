7edc92b42bbce4fb8f76874e9070fd8d3939435a
VitaliiKovalenko88 <vintilyator88@gmail.com>
2022-05-05 20:33:58 +0200

    change filter component

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,8 +1,14 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { changeFilter } from 'redux/filterSlice';
 
 export const Filter = () => {
   const dispatch = useDispatch();
+  const filter = useSelector(state => state.filter);
+
+  const onSearchInput = e => {
+    const inputValue = e.currentTarget.value;
+    dispatch(changeFilter(inputValue));
+  };
 
   return (
     <div>
